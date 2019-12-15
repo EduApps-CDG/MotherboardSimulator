ec4e28f0d881ee5605dee5b0095e257c5d54edaf
Eduardo P. Gomez <eduardoprocopiogomez@Gmail.com>
2019-12-15 03:34:05 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,31 @@
 var EBoard = {
-	canvas = document.getElementById("sim"),
-	ctx = canvas.getContext("2d"),
+	canvas: document.getElementById("sim"),
+	ctx: canvas.getContext("2d"),
+	
+	Project: function(file) {
+		var xmlhttp;
+		
+		if (window.XMLHttpRequest) {
+			xmlhttp = new XMLHttpRequest();
+		} else {
+  			xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+		}
+		
+		xmlhttp.open("GET",file,false);
+		xmlhttp.send();
+		var xmlDoc = xmlhttp.responseXML;
+		var x = xmlDoc.getElementsByTagName("project");
+		
+		this.insert = function(root, element) {
+			root.appendChild(element);
+		}
+		
+		this.download = function() {
+			document.open();
+			document.write(xmlDoc);
+			document.close();
+		}
+		
+		return x[0];
+	},
 }
